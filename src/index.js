cb79d40b7b5c1aba89447d13aa8a5af23a5f9f93
OleksandrHavrylyshyn <ol.havrylyshyn@gmail.com>
2022-01-26 13:04:59 +0200

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './style.css'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class Hello extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {color : 'FFFFFF'};
+        this.randomCol = this.randomCol.bind(this);
+    }
+    randomCol() {
+        const newColor = Math.floor(Math.random()*16777215).toString(16).toUpperCase();
+        this.setState({color: newColor})
+        document.body.style.backgroundColor = `#${newColor}`;
+    }
+    render() {
+        return (
+            <div className='block'>
+                <h2>Color hex: #{this.state.color}</h2>
+                <button onClick={this.randomCol}>Change color </button>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<Hello />, document.body)
